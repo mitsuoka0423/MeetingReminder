7080bdcdfc09de9d2ec8c2ff4dc7fa99d279dea8
Mitsuoka Takahiro <tmitsuoka0423@gmail.com>
2025-12-27 11:24:15 +0900

    Initial Commit

---
+++ MeetingReminder/MeetingReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetingReminderApp.swift
+//  MeetingReminder
+//
+//  Created by Mitsuoka Takahiro on 2025/12/27.
+//
+
+import SwiftUI
+
+@main
+struct MeetingReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MeetingReminderTests/MeetingReminderTests.swift
@@ -0,0 +1,17 @@
+//
+//  MeetingReminderTests.swift
+//  MeetingReminderTests
+//
+//  Created by Mitsuoka Takahiro on 2025/12/27.
+//
+
+import Testing
+@testable import MeetingReminder
+
+struct MeetingReminderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
